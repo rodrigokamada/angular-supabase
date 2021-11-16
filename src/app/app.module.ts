1a8b6195fc60f5c9ed83d9cf12d7573c488ff26b
Rodrigo Kamada <rodrigo@kamada.com.br>
2021-11-15 21:10:58 -0300

    Polish.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 
